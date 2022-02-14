974a80f6d1876282ac241328337823b0bc5c4c81
Bhavesh Singh <bhavesh@Bhaveshs-MacBook-Air.local>
2022-02-13 17:45:44 -0800

    Back2Basics

--- Assets/InputData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName= "Input_Data")]
-public class InputData : ScriptableObject
-{
-    public bool isPressed;
-    public bool isHeld;
-    public bool isReleased;
-}
